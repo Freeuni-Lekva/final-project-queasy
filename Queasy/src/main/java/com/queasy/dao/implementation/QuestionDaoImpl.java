6989aec9c83dd84cececbb2b6cfefba2a7170e2d
bachiSkhulukhia <bskhu18@freeuni.edu.ge>
2022-08-28 14:53:31 +0400

    change questionDao

--- Queasy/src/main/java/com/queasy/dao/implementation/QuestionDaoImpl.java
+++ Queasy/src/main/java/com/queasy/dao/implementation/QuestionDaoImpl.java
@@ -3,9 +3,15 @@ package com.queasy.dao.implementation;
 import com.queasy.dao.interfaces.ConnectionPool;
 import com.queasy.dao.interfaces.QuestionDao;
 import com.queasy.model.quiz.Question;
+import com.queasy.utility.constants.MyConstants;
+import com.queasy.utility.constants.StaticMethods;
 import com.queasy.utility.enums.QuestionType;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+import java.util.Stack;
 
 public class QuestionDaoImpl implements QuestionDao {
 
@@ -14,23 +20,54 @@ public class QuestionDaoImpl implements QuestionDao {
         this.connectionPool = connectionPool;
     }
 
-    @Override
-    public List<Question> getAllQuestions() {
-        return null;
-    }
+//    @Override
+//    public List<Question> getAllQuestions() {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Question> getAllQuestionsOf(QuestionType qt) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Question> getAllQuestionsOf(int quizId) {
+//        return null;
+//    }
+    private boolean executeUpdating(String query) {
+        Connection con = connectionPool.acquireConnection();
+        try {
+            Statement statement = con.createStatement();
+            if(statement.executeUpdate(query) > 0) {
+                connectionPool.releaseConnection(con);
+                return true;
+            }
 
-    @Override
-    public List<Question> getAllQuestionsOf(QuestionType qt) {
-        return null;
-    }
 
-    @Override
-    public List<Question> getAllQuestionsOf(int quizId) {
-        return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        connectionPool.releaseConnection(con);
+        return false;
     }
-
     @Override
     public boolean addQuestion(Question question) {
-        return false;
+        String query = "INSERT INTO " + MyConstants.QuestionsDatabaseConstants.DATABASE + " ( " +
+                        MyConstants.QuestionsDatabaseConstants.TEXT + " , " +
+                        MyConstants.QuestionsDatabaseConstants.TYPE +
+                        " , " + MyConstants.QuestionsDatabaseConstants.CREATOR_ID + " ) " +
+                        " VALUES ( '" + question.getText() + "' , '" + question.getQuestionType().toString() +
+                        "' , " + Integer.toString(question.getCreatorId());
+
+
+        return executeUpdating(query);
+    }
+
+    @Override
+    public boolean removeQuestion(int questionId) {
+        String query = "DELETE FROM " + MyConstants.QuestionsDatabaseConstants.DATABASE +
+                       "WHERE " + MyConstants.ID + " = " + questionId + " ;";
+
+        return executeUpdating(query);
     }
 }
--- Queasy/src/main/java/com/queasy/dao/interfaces/QuestionDao.java
+++ Queasy/src/main/java/com/queasy/dao/interfaces/QuestionDao.java
@@ -7,9 +7,10 @@ import java.util.List;
 //TODO: უნდა დავამატო პირველი  n  ცალი რომ ამოიღოს ან რაღაც რეინჯში
 
 public interface QuestionDao {
-    List<Question> getAllQuestions();
-    List<Question> getAllQuestionsOf(QuestionType qt);
-    List<Question> getAllQuestionsOf(int quizId);
+//    List<Question> getAllQuestions();
+//    List<Question> getAllQuestionsOf(QuestionType qt);
+//    List<Question> getAllQuestionsOf(int quizId);
 
     boolean addQuestion(Question question);
+    boolean removeQuestion(int questionId);
 }
